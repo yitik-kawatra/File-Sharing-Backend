63050db41d4115e35a9e6feb79395c20029ae300
Yitik <yitikkawatra@gmail.com>
2021-09-01 23:16:23 +0530

    fix cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 
 const app=express();
 const PORT=process.env.PORT ||3000;
-
+const cors=require('cors');
 const connectDB=require('./config/db.js');
 connectDB();
 
